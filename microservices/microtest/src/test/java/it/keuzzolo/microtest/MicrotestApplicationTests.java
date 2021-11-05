7aaf2b208e6e2ac307ad322632edd1f6436feb20
Paolo Gazzola <pgazzola@mainline.it>
2021-11-05 11:47:26 +0100

    prima release microservizio di test

---
+++ microservices/microtest/src/test/java/it/keuzzolo/microtest/MicrotestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.keuzzolo.microtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
